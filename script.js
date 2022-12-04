34f0ce1e361ed35937580ef22c82f433a84c2760
Jovic22 <109954748+Jovic22@users.noreply.github.com>
2022-12-04 06:15:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//------- Navigacija heder meni ----//
+
 const hamburger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".nav-links")
 
@@ -11,3 +13,21 @@ document.querySelectorAll(".nav-links").forEach(n => n.
         hamburger.classList.remove("active");
         navLinks.classList.remove("active");
   }))
+
+// --- otvaranje/zatvranje galerije slika i zumiranje -- //
+
+  var fullImgBox = document.getElementById("fullImgBox");
+  var fullImg = document.getElementById("fullImg");
+
+  function openfullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+  }
+
+  function closefullImg(){
+    fullImgBox.style.display = "none";
+  }
+
+
+
+
